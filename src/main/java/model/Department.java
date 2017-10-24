789762e92f8727c0cdaa37d3760babba54e9cf20
Michael Kruszczynskim <michaelkruszczynski@yahoo.co.uk>
2017-10-24 23:35:25 +0100

    Classes added

---
+++ src/main/java/model/Department.java
@@ -0,0 +1,39 @@
+package model;
+
+public class Department {
+	
+ private double hoursUsedForWeek;
+ private String departmentName;
+ private double hoursBooked;
+ private String dayDue;
+ 
+  public double getHoursUsedForWeek() {
+	return hoursUsedForWeek;
+  }
+  public void setHoursUsedForWeek(double hoursUsedForWeek) {
+	this.hoursUsedForWeek = hoursUsedForWeek;
+  }
+  public String getDepartmentName() {
+	return departmentName;
+  }
+  public void setDepartmentName(String departmentName) {
+	this.departmentName = departmentName;
+  }
+  public double getHoursBooked() {
+	return hoursBooked;
+  }
+  public void setHoursBooked(double hoursBooked) {
+	this.hoursBooked = hoursBooked;
+  }
+  public String getDayDue() {
+	return dayDue;
+  }
+  public void setDayDue(String dayDue) {
+	this.dayDue = dayDue;
+}
+ 
+  
+  
+  
+
+}
